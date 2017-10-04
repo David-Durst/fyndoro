d3ec6f3c90b25c8b14ef7ac6f40459d05b6267d1
David Durst <davidbdurst@gmail.com>
2017-10-04 16:41:07 -0700

    removing dead code

--- uncertain/imageCleaningAndGoogleSearching/scrape.py
+++ uncertain/imageCleaningAndGoogleSearching/scrape.py
@@ -29,25 +29,13 @@ with Browser() as browser:
         #wait_time = randint(4,6)
         # get the reverse image section open
         browser.find_by_id("qbi").click()
-        # go from url reverse image to file upload image
-        #browser.find_by_text("Upload an image").click()
         # upload a file
         browser.attach_file("encoded_image", join(dirToDownload, imgFileName))
-        #print("start")
-        #print(dirToDownload)
-        #print(imgFileName)
-        #print("b1")
-        #print(browser.html)
-        #print("b2")
         visSimLink = browser.find_by_text("Visually similar images")
-        print(browser.url)
-        #print(visSimLink)
-        #print(browser.html)
         if len(visSimLink) == 0:
             continue
         visSimLink.click()
         images = [x['href'] for x in browser.find_by_css(".rg_l")]
-        #images = browser.find_by_css(".rg_ic.rg_i")
         numDownloaded = 0
 
         print("images to download " + str(len(images)))
@@ -57,29 +45,12 @@ with Browser() as browser:
         for image in images:
             if numDownloaded > imagesToDownloadPerImage:
                 break
-            #image.click()
-            #browser.find_by_css(".irc_fsl.irc_but.i3596")[1].click()
-            #time.sleep(0.5)
-            #imgToDownload = browser.windows[1].url
-            #print(imgToDownload)
-            #time.sleep(0.5)
-            #imgToDownload = browser.windows[1].url
             browser.visit(image)
-            #print(browser.url)
             if len(browser.find_by_css(".irc_fsl.irc_but.i3596")) == 0:
                 continue
             imgToDownload = browser.find_by_css(".irc_fsl.irc_but.i3596")[1]['href']
-            #print("a")
-            #try:
-            #    browser.visit(browser.find_by_css(".irc_fsl.irc_but.i3596")[1]['href'])
-            #except:
-            #    pass
-            #imgToDownload = browser.windows[0].url
-            #print(imgToDownload)
-            #process = subprocess.Popen("wget -t 3 --directory-prefix " + outputDir + " " + imgToDownload, shell=True)
-            print("timeout 15 python " + scriptDir + "/scrapeDLSubprocess.py " + imgToDownload + " " + outputDir)
-            process = subprocess.Popen("timeout 15 python " + scriptDir + "/scrapeDLSubprocess.py " + imgToDownload + " " + outputDir, shell=True)
-            #browser.windows[1].close()
+            print("timeout 15 python \"" + scriptDir + "/scrapeDLSubprocess.py\" \"" + imgToDownload + "\" " + outputDir)
+            process = subprocess.Popen("timeout 15 python \"" + scriptDir + "/scrapeDLSubprocess.py\" \"" + imgToDownload + "\" " + outputDir, shell=True)
             processList.append(process)
             numDownloaded += 1
 
