f82332e0b642a201c137a17ada5188cc02e0f053
David Durst <davidbdurst@gmail.com>
2017-10-08 17:43:48 -0700

    addessing bug that wget can't take both output dir and name

--- uncertain/downloadImagenet.sh
+++ uncertain/downloadImagenet.sh
@@ -6,9 +6,9 @@ output_dir=$(readlink -f "$2")
 name=$3
 api_key=$(cat $scriptDir/../IMAGENET_API_KEY)
 
-wget --directory-prefix $output_dir -O ${name}.tar "http://www.image-net.org/download/synset?wnid=${wnid}&username=durst&accesskey=${api_key}&release=latest&src=stanford"
+wget -O ${name}.tar "http://www.image-net.org/download/synset?wnid=${wnid}&username=durst&accesskey=${api_key}&release=latest&src=stanford"
 mkdir -p $output_dir/$name
-mv $output_dir/${name}.tar $output_dir/$name/
+mv ${name}.tar $output_dir/$name/
 cd $output_dir/$name/
 tar -xf ${name}.tar
 rm ${name}.tar
