d5f7f53fab5f2154a09e64af37c655b5052b7aec
David Durst <davidbdurst@gmail.com>
2017-10-08 17:21:51 -0700

    download imagnet script uses output dir option

--- uncertain/downloadImagenet.sh
+++ uncertain/downloadImagenet.sh
@@ -5,4 +5,4 @@ wnid=$1
 output_dir=$(readlink -f "$2")
 api_key=$(cat $scriptDir/../IMAGENET_API_KEY)
 
-wget http://www.image-net.org/download/synset?wnid=${wnid}&username=durst&accesskey=${api_key}release=latest&src=stanford
+wget --directory-prefix $output_dir http://www.image-net.org/download/synset?wnid=${wnid}&username=durst&accesskey=${api_key}release=latest&src=stanford
