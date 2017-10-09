6f1ce8994dfc893d57dc04dc5f11e302b18d64a0
David Durst <davidbdurst@gmail.com>
2017-10-08 17:21:00 -0700

    download imagenet script added

---
+++ uncertain/downloadImagenet.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# first input is the wnid to download. Second input is the location to download it to
+scriptDir=$(dirname "$(readlink -f "$0")")
+wnid=$1
+output_dir=$(readlink -f "$2")
+api_key=$(cat $scriptDir/../IMAGENET_API_KEY)
+
+wget http://www.image-net.org/download/synset?wnid=${wnid}&username=durst&accesskey=${api_key}release=latest&src=stanford
