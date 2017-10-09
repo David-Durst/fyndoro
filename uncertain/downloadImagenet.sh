2876e24d3a4fedec3a37f43d2f7c25faf6ec651c
David Durst <davidbdurst@gmail.com>
2017-10-08 17:34:35 -0700

    download imagenet script now cleans up after itself

--- uncertain/downloadImagenet.sh
+++ uncertain/downloadImagenet.sh
@@ -3,6 +3,13 @@
 scriptDir=$(dirname "$(readlink -f "$0")")
 wnid=$1
 output_dir=$(readlink -f "$2")
+name=$3
 api_key=$(cat $scriptDir/../IMAGENET_API_KEY)
 
-wget --directory-prefix $output_dir "http://www.image-net.org/download/synset?wnid=${wnid}&username=durst&accesskey=${api_key}&release=latest&src=stanford"
+wget --directory-prefix $output_dir -O ${name}.tar "http://www.image-net.org/download/synset?wnid=${wnid}&username=durst&accesskey=${api_key}&release=latest&src=stanford"
+mkdir $output_dir/$name
+mv $output_dir/${name}.tar $output_dir/
+cd $output_dir
+tar -xf ${name}.tar
+rm ${name}.tar
+cd -
