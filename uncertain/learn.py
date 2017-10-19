3175588034ad44ee40cb230b20b9a4f60ad0da8a
David Durst <davidbdurst@gmail.com>
2017-10-19 14:53:55 -0700

    going back to linear at last layer, need it there as need a mapping from high dimensional space to class space that is tunable

--- uncertain/learn.py
+++ uncertain/learn.py
@@ -150,7 +150,7 @@ model_ft = models.resnet18(pretrained=True)
 for param in model_ft.parameters():
     param.requires_grad = False
 num_ftrs = model_ft.fc.in_features
-model_ft.fc = nn.Softmax(num_ftrs, 2)
+model_ft.fc = nn.Linear(num_ftrs, 2)
 
 if use_gpu:
     model_ft = model_ft.cuda()
