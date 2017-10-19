4f64c96fba0157e9ae27dfb5bae2bb2de1e9339e
David Durst <davidbdurst@gmail.com>
2017-10-19 15:14:35 -0700

    data to label mapping should work

--- uncertain/createDataToLabelMapping.py
+++ uncertain/createDataToLabelMapping.py
@@ -7,14 +7,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
-import torchvision
 from torchvision import datasets, models, transforms
-import numpy as np
-import time
-import copy
-from uncertain.uncertainCrossEntropyLoss import UncertainCrossEntropyLoss
+import torch.nn.functional as F
 import sys
 import os
+import pprint
 
 # inputs should be directory_of_data number_positive_examples output_file model_output_dir
 data_dir = sys.argv[1]
@@ -47,18 +44,19 @@ model = models.resnet18(pretrained=True)
 for param in model.parameters():
     param.requires_grad = False
 num_ftrs = model.fc.in_features
-model.fc = nn.LogSoftmax(num_ftrs, 2)
+model.fc = nn.Linear(num_ftrs, 2)
 model.load_state_dict(torch.load(model_input_location))
 
 if use_gpu:
     model = model.cuda()
 
+outputs = []
 for phase in ['train', 'val']:
     print("In phase " + phase)
     phaseLen = str(len(dsets[phase]))
     i = 0
+    outputs[phase] = []
     for data in dsets[phase]:
-        i += 1
         print("Working on element " + str(i) + " of " + phaseLen + " in phase " + phase)
         inputs, labelIndices = data
         inputs = inputs.unsqueeze(0)
@@ -68,118 +66,8 @@ for phase in ['train', 'val']:
         else:
             inputs = Variable(inputs)
         # based on __getitem__ implementation of datasets.ImageLoader, imgs index matches that of items
-        outputs = model(inputs)
-
-dset_loaders = {x: torch.utils.data.DataLoader(dsets[x], batch_size=4,
-                                               shuffle=True, num_workers=4)
-                for x in ['train', 'val']}
-dset_sizes = {x: len(dsets[x]) for x in ['train', 'val']}
-dset_classes = dsets['train'].classes
-
-
-# Get a batch of training data
-inputs, classes = next(iter(dset_loaders['train']))
-
-# Make a grid from batch
-out = torchvision.utils.make_grid(inputs)
-
-def classIndexToProbability(classIdx, class_to_idx_map):
-    idx_to_class = {v: k for k, v in class_to_idx_map.items()}
-    return [float(x) for x in idx_to_class[classIdx].split(",")]
-
-def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=25):
-    since = time.time()
-
-    best_model = model
-    best_acc = 0.0
-
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
-
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                optimizer = lr_scheduler(optimizer, epoch)
-                model.train(True)  # Set model to training mode
-            else:
-                model.train(False)  # Set model to evaluate mode
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for data in dset_loaders[phase]:
-                # get the inputs
-                inputs, labelIndices = data
-
-                labelProbabilities = torch.FloatTensor([classIndexToProbability(index, dsets[phase].class_to_idx) for index in labelIndices])
-
-                # wrap them in Variable
-                if use_gpu:
-                    inputs, labelProbabilitiesVar = Variable(inputs.cuda()), \
-                        Variable(labelProbabilities.cuda())
-                else:
-                    inputs, labelProbabilitiesVar = Variable(inputs), Variable(labelProbabilities)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                outputs = model(inputs)
-                _, preds = torch.max(outputs.data, 1)
-                loss = criterion(outputs, labelProbabilitiesVar)
-
-                # backward + optimize only if in training phase
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
-
-                # the correct label is the one with greatest probability
-                _, labels = torch.max(labelProbabilitiesVar, dim=1)
-                # statistics
-                running_loss += loss.data[0]
-                running_corrects += torch.sum(preds == labels.data)
-
-            epoch_loss = running_loss / dset_sizes[phase]
-            epoch_acc = running_corrects / dset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model = copy.deepcopy(model)
-
-        print()
-
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
-    return (best_model, best_acc)
-
-
-def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
-    """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
-    lr = init_lr * (0.1**(epoch // lr_decay_epoch))
-
-    if epoch % lr_decay_epoch == 0:
-        print('LR is set to {}'.format(lr))
-
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
-    return optimizer
-
-
-criterion = UncertainCrossEntropyLoss()
-
-model_ft, best_acc = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=25)
+        outputs[phase][data.imgs[i][0]] = F.softmax(model(inputs))
+        i += 1
 
 with open(output_file, 'a') as f:
-    f.write(data_dir + "," + num_training_str + "," + str(best_acc) + "\n")
-
-torch.save(model_ft.state_dict(), model_output_dir + "/" + num_training_str)
+    pprint.pprint(outputs, stream=f)
