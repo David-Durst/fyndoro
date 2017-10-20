4174f43e06d68ae15ea45fde05f2c25e2ef5475a
David Durst <davidbdurst@gmail.com>
2017-10-20 12:01:01 -0700

    have little helper for loading models from csv

---
+++ uncertain/loadAnalyzeModel.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def loadModel(modelLocation):
+    with open(modelLocation, 'r') as f:
+        trainAndVal = f.read()
+        results = {}
+        results["train"] = pd.DataFrame.from_dict(trainAndVal["train"])
+        results["val"] = pd.DataFrame.from_dict(trainAndVal["val"])
+        return results
