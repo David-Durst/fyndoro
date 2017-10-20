403af3147355d3537b3cd86528fc0baf52d3cfb0
David Durst <davidbdurst@gmail.com>
2017-10-20 13:33:52 -0700

    actually converting string to dict in loading model results

--- uncertain/loadAnalyzeModel.py
+++ uncertain/loadAnalyzeModel.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def loadModel(modelLocation):
     with open(modelLocation, 'r') as f:
-        trainAndVal = f.read()
+        trainAndVal = eval(f.read())
         results = {}
         results["train"] = pd.DataFrame.from_dict(trainAndVal["train"])
         results["val"] = pd.DataFrame.from_dict(trainAndVal["val"])
