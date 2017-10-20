80c5bf13730567ec8e98501e550e6b3e33051c48
David Durst <davidbdurst@gmail.com>
2017-10-20 14:39:59 -0700

    added ability to output lots of useful types of images

--- uncertain/loadAnalyzeModel.py
+++ uncertain/loadAnalyzeModel.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 def loadModel(modelLocation):
     with open(modelLocation, 'r') as f:
@@ -7,3 +8,38 @@ def loadModel(modelLocation):
         results["train"] = pd.DataFrame.from_dict(trainAndVal["train"])
         results["val"] = pd.DataFrame.from_dict(trainAndVal["val"])
         return results
+
+
+def uncertain(valAndTrain):
+    val = valAndTrain["val"]
+    valUncertain = val[(val[2] > 0.45) & (val[2] < 0.55)][0].tolist()
+    for fName in valUncertain:
+        os.system("cp " + fName + " mOrFRecNoFreezeModels/uncertainPics/val/" + os.path.basename(fName))
+
+    train = valAndTrain["train"]
+    trainUncertain = train[(train[2] > 0.45) & (train[2] < 0.55)][0].tolist()
+    for fName in trainUncertain:
+        os.system("cp " + fName + " mOrFRecNoFreezeModels/uncertainPics/train/" + os.path.basename(fName))
+
+
+def certainRight(valAndTrain):
+    val = valAndTrain["val"]
+    valUncertain = val[val[2] > 0.90][0].tolist()
+    for fName in valUncertain:
+        os.system("cp " + fName + " mOrFRecNoFreezeModels/certainPics/right/val/" + os.path.basename(fName))
+
+    train = valAndTrain["train"]
+    trainUncertain = train[train[1] > 0.90][0].tolist()
+    for fName in trainUncertain:
+        os.system("cp " + fName + " mOrFRecNoFreezeModels/certainPics/right/train/" + os.path.basename(fName))
+
+def certainWrong(valAndTrain):
+    val = valAndTrain["val"]
+    valUncertain = val[val[1] > 0.90][0].tolist()
+    for fName in valUncertain:
+        os.system("cp " + fName + " mOrFRecNoFreezeModels/certainPics/right/val/" + os.path.basename(fName))
+
+    train = valAndTrain["train"]
+    trainUncertain = train[train[2] > 0.90][0].tolist()
+    for fName in trainUncertain:
+        os.system("cp " + fName + " mOrFRecNoFreezeModels/certainPics/right/train/" + os.path.basename(fName))
