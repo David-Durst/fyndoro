b70be56157db7723657cc873e741a1524483ba4f
David Durst <davidbdurst@gmail.com>
2017-10-20 15:19:21 -0700

    actually splitting right and wrong

--- uncertain/loadAnalyzeModel.py
+++ uncertain/loadAnalyzeModel.py
@@ -22,24 +22,35 @@ def uncertain(valAndTrain):
         os.system("cp " + fName + " mOrFRecNoFreezeModels/uncertainPics/train/" + os.path.basename(fName))
 
 
-def certainRight(valAndTrain):
-    val = valAndTrain["val"]
-    valUncertain = val[val[2] > 0.90][0].tolist()[:100]
-    for fName in valUncertain:
-        os.system("cp " + fName + " mOrFRecNoFreezeModels/certainPics/right/val/" + os.path.basename(fName))
+def certainRightAndWrong(valAndTrain):
+    for type in ['train', 'val']:
+        dset = valAndTrain[type]
+        certain = dset[(dset[1] > 0.90) | (dset[1] < 0.1)][600]
 
-    train = valAndTrain["train"]
-    trainUncertain = train[train[1] > 0.90][0].tolist()[:100]
-    for fName in trainUncertain:
-        os.system("cp " + fName + " mOrFRecNoFreezeModels/certainPics/right/train/" + os.path.basename(fName))
+        destDirRightCat1 = " mOrFRecNoFreezeModels/certainPics/right/1.0,0.0/" + type + "/"
+        os.system("mkdir -p " + destDirRightCat1)
+        certainRightCat1 = certain[(dset(1) > 0.9) & (dset(0).str.contains("1.0,0.0"))]
+        for fname in certainRightCat1:
+            os.system(
+                "cp " + fname + destDirRightCat1 + os.path.basename(fname))
 
-def certainWrong(valAndTrain):
-    val = valAndTrain["val"]
-    valUncertain = val[val[1] > 0.90][0].tolist()[:100]
-    for fName in valUncertain:
-        os.system("cp " + fName + " mOrFRecNoFreezeModels/certainPics/wrong/val/" + os.path.basename(fName))
+        destDirRightCat2 = " mOrFRecNoFreezeModels/certainPics/right/0.0,1.0/" + type + "/"
+        os.system("mkdir -p " + destDirRightCat2)
+        certainRightCat2 = certain[(dset(2) > 0.9) & (dset(0).str.contains("0.0,1.0"))]
+        for fname in certainRightCat2:
+            os.system(
+                "cp " + fname + destDirRightCat2 + os.path.basename(fname))
 
-    train = valAndTrain["train"]
-    trainUncertain = train[train[2] > 0.90][0].tolist()[:100]
-    for fName in trainUncertain:
-        os.system("cp " + fName + " mOrFRecNoFreezeModels/certainPics/wrong/train/" + os.path.basename(fName))
+        destDirWrongCat1 = " mOrFRecNoFreezeModels/certainPics/wrong/1.0,0.0/" + type + "/"
+        os.system("mkdir -p " + destDirWrongCat1)
+        certainWrongCat1 = certain[(dset(1) < 0.1) & (dset(0).str.contains("1.0,0.0"))]
+        for fname in certainWrongCat1:
+            os.system(
+                "cp " + fname + destDirWrongCat1 + os.path.basename(fname))
+
+        destDirWrongCat2 = " mOrFRecNoFreezeModels/certainPics/wrong/0.0,1.0/" + type + "/"
+        os.system("mkdir -p " + destDirWrongCat2)
+        certainWrongCat2 = certain[(dset(2) < 0.1) & (dset(0).str.contains("0.0,1.0"))]
+        for fname in certainWrongCat2:
+            os.system(
+                "cp " + fname + destDirWrongCat2 + os.path.basename(fname))
