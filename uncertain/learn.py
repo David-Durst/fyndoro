3f685be4881d0f0556ae2caa9452966ce6c1c592
David Durst <davidbdurst@gmail.com>
2017-10-21 20:41:37 -0700

    getting float out of variable for printing

--- uncertain/learn.py
+++ uncertain/learn.py
@@ -122,7 +122,7 @@ def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=25):
                 phase, epoch_loss, epoch_acc))
 
             print('{:.7f}: sum of abs of difference in weights'.format(
-                (final_layer_weights_last_iteration - model.fc.weight).abs().sum()))
+                (final_layer_weights_last_iteration - model.fc.weight).abs().sum().data[0]))
             final_layer_weights_last_iteration = model.fc.weight
 
             # deep copy the model
