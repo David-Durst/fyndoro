25215e9b1212020c86762f749f0abc826efb97b6
David Durst <davidbdurst@gmail.com>
2017-10-21 21:49:34 -0700

    using clone in both places now

--- uncertain/learn.py
+++ uncertain/learn.py
@@ -65,7 +65,7 @@ def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=25):
 
     best_model = model
     best_acc = 0.0
-    final_layer_weights_last_iteration = model.fc.weight
+    final_layer_weights_last_iteration = model.fc.weight.clone()
 
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
