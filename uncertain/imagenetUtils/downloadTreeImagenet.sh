ac47b048f064fb82526d819a017e57cbe9fbb1ad
David Durst <davidbdurst@gmail.com>
2017-11-03 16:27:28 -0700

    stripping carriage return and naming folders right

--- uncertain/imagenetUtils/downloadTreeImagenet.sh
+++ uncertain/imagenetUtils/downloadTreeImagenet.sh
@@ -8,7 +8,8 @@ treeFile=${name}tree.txt
 wget -O $treeFile "http://www.image-net.org/api/text/wordnet.structure.hyponym?wnid=${wnid}&full=1"
 #get rid of starting - and any trailing spaces in each line
 sed -i 's/-//g' $treeFile
+sed -i 's/\r//g' $treeFile
 sed -i 's/[[:blank:]]*$//g' $treeFile
 while read wnid; do
-  ${scriptDir}/downloadImagenet.sh $wnid $2/$wnid $3
+  ${scriptDir}/downloadImagenet.sh $wnid $2/$name $wnid
 done < $treeFile
