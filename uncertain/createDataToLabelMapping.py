a8b7f205493388f11cbb5ef19380350e644de794
David Durst <davidbdurst@gmail.com>
2017-11-08 13:43:31 -0800

    when creating data to label mapping, treat training data like yuo treat val data

--- uncertain/createDataToLabelMapping.py
+++ uncertain/createDataToLabelMapping.py
@@ -19,11 +19,12 @@ model_input_location = sys.argv[2]
 output_file = sys.argv[3]
 
 # Data augmentation and normalization for training
-# Just normalization for validation
+# do transforms that are normally just for validation
+# for all data
 data_transforms = {
     'train': transforms.Compose([
-        transforms.RandomSizedCrop(224),
-        transforms.RandomHorizontalFlip(),
+        transforms.Scale(256),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
