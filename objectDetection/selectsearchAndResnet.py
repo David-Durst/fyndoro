6a4faad53cd3b00753c56f00f2a277aa3dba49ae
David Durst <davidbdurst@gmail.com>
2017-11-17 15:10:04 -0800

    if a bounding box has high probability, take the max prob one, otherwise drop the example

--- objectDetection/selectsearchAndResnet.py
+++ objectDetection/selectsearchAndResnet.py
@@ -16,7 +16,7 @@ import json
 data_dir = sys.argv[1]
 # if this is empty, the default model will be used
 model_input_location = sys.argv[2]
-output_file = sys.argv[3]
+output_dir = sys.argv[3]
 
 # Data augmentation and normalization for training
 # do transforms that are normally just for validation
@@ -43,26 +43,35 @@ if model_input_location != "":
 
 model = model.cuda()
 
+# take in the bounds from selective search (left, top, width, height)
+# and crop the image using them
+def cropImageUsingBounds(image, bounds):
+    # cropCoordinates is in format left, upper, right, lower
+    # which pillow's crop wants
+    cropCoordinatesPillow = (bounds[0], bounds[1],
+                             bounds[0] + bounds[2],
+                             bounds[1] + bounds[3])
+
+    return image.crop(cropCoordinatesPillow)
+
+
 outputs = []
+i = 0
 for dataPoint in dset:
-    #print("Working on element " + str(i) + " of " + phaseLen + " in phase " + phase)
+    print("Working on element " + str(i) + " of " + len(dset))
     image, labelIndices = dataPoint
     # just using default parameters, will tune later
     img_lbl, regions = selectivesearch.selective_search(numpy.asarray(image), scale=500, sigma=0.9, min_size=10)
     imageRegionsAsTensors = []
     for region in regions:
-        # cropBounds is in format left, top, width, height
-        cropBoundsSelectSearch = region['rect']
         # skip empty regions
-        if cropBoundsSelectSearch[2] == 0 or cropBoundsSelectSearch[3] == 0:
+        if region['rect'][2] == 0 or region['rect'][3] == 0:
             continue
-        # cropCoordinates is in format left, upper, right, lower
-        # which pillow's crop wants
-        cropCoordinatesPillow = (cropBoundsSelectSearch[0], cropBoundsSelectSearch[1],
-                                 cropBoundsSelectSearch[0] + cropBoundsSelectSearch[2],
-                                 cropBoundsSelectSearch[1] + cropBoundsSelectSearch[3])
+        # skip regions of fewer than 2000 pixels
+        if region['size'] < 2000:
+            continue
 
-        imageRegion = image.crop(cropCoordinatesPillow)
+        imageRegion = cropImageUsingBounds(region['rect'])
         imageAsTensorForEval = data_transforms(imageRegion)
         imageRegionsAsTensors.append(imageAsTensorForEval)
 
@@ -74,9 +83,18 @@ for dataPoint in dset:
         inputs = Variable(imageRegionsAsOneTensor)
     # based on __getitem__ implementation of datasets.ImageLoader, imgs index matches that of items
     classProbabilityTensor = F.softmax(model(inputs)).data
-    # view -1 to remove the many, unnecessary dimensions that
-    # have 1 value
-    if use_gpu:
-        classProbability = classProbabilityTensor.cpu().numpy()[0]
-    else:
-        classProbability = classProbabilityTensor.numpy()[0]
+    # take the region with the max probability of being the desired
+    # class
+    # max(0) gives the indices and values of the max in the first
+    # dimension fo tensor, which is max probability of being in
+    # each category
+    mostLikely = classProbabilityTensor.max(0)
+    # first [0] gives the probabilities. skip if p < 0.9
+    # second [0] gives the index of the element with the max probability
+    # of being the first class (0 indexing)
+    if mostLikely[0][0] < 0.9:
+        continue
+    # [1] gives the indices instead of the probabilities
+    indexOfMostLikely = classProbabilityTensor.max(0)[1][0]
+    fileName = os.path.basename(dset.imgs[i][0])
+    cropImageUsingBounds(image, regions[indexOfMostLikely]['rect']).save(output_dir + "/" + fileName)
