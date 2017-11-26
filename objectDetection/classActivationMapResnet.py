40847fdaa62ca2c3077c44b7dd5ce9b172109673
David Durst <davidbdurst@gmail.com>
2017-11-25 16:06:55 -0800

    made class activation map take desired inputs, use my model. also wrote script for copying random subset of a folder to another folder

--- objectDetection/classActivationMapResnet.py
+++ objectDetection/classActivationMapResnet.py
@@ -1,31 +1,38 @@
 # simple implementation of CAM in PyTorch for the networks such as ResNet, DenseNet, SqueezeNet, Inception
 # from https://github.com/metalbubble/CAM/blob/master/pytorch_CAM.py
 
-import io
-import requests
+import sys
+import json
 from PIL import Image
 from torchvision import models, transforms
 from torch.autograd import Variable
 from torch.nn import functional as F
+from torch import nn
+import torch
 import numpy as np
 import cv2
 
-# input image
-LABELS_URL = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
-IMG_URL = 'http://media.mlive.com/news_impact/photo/9933031-large.jpg'
+# if this is empty, the default model will be used
+model_input_location = sys.argv[1]
+input_image = sys.argv[2]
+label_map = sys.argv[3]
+desired_label_index = int(sys.argv[4])
+output_location = sys.argv[4]
 
-# networks such as googlenet, resnet, densenet already use global average pooling at the end, so CAM could be used directly.
-model_id = 1
-if model_id == 1:
-    net = models.squeezenet1_1(pretrained=True)
-    finalconv_name = 'features' # this is the last conv layer of the network
-elif model_id == 2:
-    net = models.resnet18(pretrained=True)
-    finalconv_name = 'layer4'
-elif model_id == 3:
-    net = models.densenet161(pretrained=True)
-    finalconv_name = 'features'
 
+net = models.resnet18(pretrained=True)
+#for param in model.parameters():
+#    param.requires_grad = False
+num_ftrs = net.fc.in_features
+net.fc = nn.Linear(num_ftrs, 2)
+# if model_input_location is empty, use default weights
+if model_input_location != "":
+    # https://discuss.pytorch.org/t/on-a-cpu-device-how-to-load-checkpoint-saved-on-gpu-device/349/3
+    # no idea what the second and third parameters to torch.load do,
+    # but they fix issue of loading model trained on gpu on a host with only cpu, from the link above
+    net.load_state_dict(torch.load(model_input_location, map_location=lambda storage, loc: storage))
+
+finalconv_name = 'layer4'
 net.eval()
 
 # hook the feature extractor
@@ -64,31 +71,28 @@ preprocess = transforms.Compose([
    normalize
 ])
 
-response = requests.get(IMG_URL)
-img_pil = Image.open(io.BytesIO(response.content))
-img_pil.save('test.jpg')
-
+img_pil = Image.open(input_image)
 img_tensor = preprocess(img_pil)
 img_variable = Variable(img_tensor.unsqueeze(0))
 logit = net(img_variable)
 
 # download the imagenet category list
 classes = {int(key):value for (key, value)
-          in requests.get(LABELS_URL).json().items()}
+          in json.load(open(label_map)).items()}
 
 h_x = F.softmax(logit).data.squeeze()
 probs, idx = h_x.sort(0, True)
 
 # output the prediction
-for i in range(0, 5):
+for i in range(0, 2):
     print('{:.3f} -> {}'.format(probs[i], classes[idx[i]]))
 
 # generate class activation mapping for the top1 prediction
-CAMs = returnCAM(features_blobs[0], weight_softmax, [idx[0]])
+CAMs = returnCAM(features_blobs[0], weight_softmax, [desired_label_index])
 
 # render the CAM and output
-print('output CAM.jpg for the top1 prediction: %s'%classes[idx[0]])
-img = cv2.imread('test.jpg')
+print('showing heatmap for label %s'%classes[desired_label_index])
+img = cv2.imread(input_image)
 height, width, _ = img.shape
 heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
 result = heatmap * 0.3 + img * 0.5
