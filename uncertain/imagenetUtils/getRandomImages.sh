6d83db13c8f877413643f87b4028c8bbd0cf3095
David Durst <davidbdurst@gmail.com>
2017-11-25 16:12:23 -0800

    using input_dir variable for random image copier

--- uncertain/imagenetUtils/getRandomImages.sh
+++ uncertain/imagenetUtils/getRandomImages.sh
@@ -3,6 +3,6 @@ input_dir=$(readlink -f "$1")
 output_dir=$(readlink -f "$2")
 num_to_copy=$3
 
-for d in */; do
+for d in ${input_dir}/*/; do
     shuf -n $num_to_copy -e $d/* | xargs -I {} cp {} $output_dir
 done
